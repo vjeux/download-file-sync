68bf6a1be31023d3707a923a5596ac6a027f7c48
Christopher Chedeau <vjeux@fb.com>
2015-11-21 09:56:45 -0800

    silent

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = function(url) {
   return require('child_process')
-    .execFileSync('curl', ['-L', url], {encoding: 'utf8'});
+    .execFileSync('curl', ['--silent', '-L', url], {encoding: 'utf8'});
 }
